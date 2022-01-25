780e27db383f1c4976f6f306099b5ec11fde76f4
Sara Dolganov <sclinede@gmail.com>
2022-01-25 15:40:02 +0100

    Fix env assignment with `export ENV`

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 if [ "$SPIN" ]; then
-  echo "EDITOR=vim" >> ~/.zshrc
+  echo "export EDITOR=vim" >> ~/.zshrc
   ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
   if ! command -v rg &> /dev/null; then
     sudo apt-get install -y tig
