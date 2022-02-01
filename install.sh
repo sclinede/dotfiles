ee6b60cdf533e13e55145e6121a2a00847698d0f
Sara Dolganov <sclinede@gmail.com>
2022-02-01 12:23:52 +0100

    Update install.sh
    Adds AG for file search and solargraph for code navigation

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 if [ "$SPIN" ]; then
   echo "export EDITOR=vim" >> ~/.zshrc
   ln -sf ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+  
   if ! command -v rg &> /dev/null; then
-    sudo apt-get install -y tig
+    sudo apt-get install -y tig ag
+    sudo gem install solargraph
   fi
 fi
 
