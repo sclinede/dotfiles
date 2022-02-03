0b5ebfdaf6b929a75fd9c08549614499f1930bc5
Sara Dolganov <sclinede@gmail.com>
2022-02-03 18:06:22 +0100

    Fix package name for 'ag'

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if [ "$SPIN" ]; then
     sudo apt-get install -y tig
   fi
   if ! command -v ag &> /dev/null; then
-    sudo apt-get install -y ag    
+    sudo apt-get install -y silversearcher-ag    
   fi
   sudo gem install solargraph
 fi
